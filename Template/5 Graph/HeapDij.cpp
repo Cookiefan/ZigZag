8adddad7d9b57ae4fe790c32eb6f57e865a15328
cookie  <cookie@hit.edu.cn>
2019-07-22 22:22:41 +0800

    sap

--- Template/5 Graph/HeapDij.cpp
+++ Template/5 Graph/HeapDij.cpp
@@ -1,40 +1,20 @@
-//堆DJ
-struct node
-{
-	int id,dis;
-	node(){}
-	node(int x,int y)
-	{
-		id=x; dis=y;
-	}
-	friend bool operator <(node a,node b)
-	{ 
-		return a.dis>b.dis;
-	}
-};
-
-priority_queue<node> q;
-
 void dij(int st)
 {
-	for (int i=1;i<=n;i++) dis[i]=inf;
+	memset(d, 0x3f, sizeof(d));
 	memset(vis,0,sizeof(vis));
-	while (!q.empty()) q.pop();
-	q.push(node(st,0));
-	dis[st]=0;
-	while (!q.empty())
-	{
-		int i=q.top().id;
+	priority_queue<pii, vector<pii>, greater<pii> > q;
+	q.push(pii(0, st));
+	d[st]=0;
+	while (!q.empty()){
+		int i=q.top().second;
 		q.pop();
 		if (vis[i]) continue;
 		vis[i]=1;
-		for (int j=fir[i];j;j=e[j].next)
-		{
+		for (int j=fir[i];j;j=e[j].next){
 			int k=e[j].t;
-			if (!vis[k]&&dis[i]+e[j].val<dis[k])
-			{
-				dis[k]=dis[i]+e[j].val;
-				q.push(node(k,dis[k]));
+			if (d[i]+e[j].val<d[k]){
+				d[k]=d[i]+e[j].val;
+				q.push(pii(d[k], k));
 			}
 		}
 	}
